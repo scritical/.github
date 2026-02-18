b3fc7d0983bddaa9e922ae01ed52c95c1b0fd4f3
sseraj <sseraj@umich.edu>
2026-02-18 21:06:18 +0000

    remove pre-commit and pylint config files

--- generate-pylintrc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-pylint \
---disable all \
---enable basic,classes,exceptions,imports,newstyle,refactoring,stdlib,string,typecheck,variables \
---disable C,R,I,unspecified-encoding,protected-access,import-error \
---generate-rcfile \
-> .pylintrc
